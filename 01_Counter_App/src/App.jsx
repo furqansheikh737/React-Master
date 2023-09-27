c0d432e7c61e0eaed3f38937151eceff5f79fba8
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-09-28 00:14:09 +0500

    add tailwind css

--- 01_Counter_App/src/App.jsx
+++ 01_Counter_App/src/App.jsx
@@ -4,14 +4,29 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const addValue = () => {
+    if (count >= 20) {
+    } else {
+      setCount(count + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (count <= 0) {
+    } else {
+      setCount(count - 1)
+    }
+  }
+
   return (
-    <div className='main'>
-      <h1>Counter value: 5</h1>
-      
-      <button>Add value</button>
-      <br/>
-      <button>Delete value</button>
-    </div>
+    <>
+      <div className='rounded p-16 bg-black'>
+        <h1 className='text-3xl text-white text-center font-bold'>Counter App: {count}</h1>
+        <button onClick={addValue} className='mt-4 ml-11 p-4 rounded bg-gray-500 text-white'>Increment</button>
+        <br />
+        <button onClick={removeValue} className='mt-4 ml-11 p-4  rounded bg-gray-500 text-white'>decrement</button>
+      </div>
+    </>
   )
 }
 
